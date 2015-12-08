30d172e273684b389c44500e05ab1ce0d0cb6a1f
samover <samuel.overloop@gmail.com>
2015-12-08 16:29:54 +0000

    integrate firebase

--- app/scripts/controllers/buttonController.js
+++ app/scripts/controllers/buttonController.js
@@ -6,7 +6,15 @@ angular.module('SoSafe')
     self.status = {
       message: 'Are you ok?'
     };
+
     self.sendRequest = function(){
+      var requestRef = new Firebase('https://sosafe.firebaseio.com');
+      var child = requestRef.child('requests');
+      var sender = 'Radu';
+      var receivers = ['Sam', 'Andrew'];
+
+      child.push({ "sender": sender, "receivers": receivers });
+
       self.status.message = 'Waiting for response';
     };
   });
