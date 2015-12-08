d2570f5bd9ba09cf7a2ba80f22a8eabe5e45809c
hasulica <raduasandei@gmail.com>
2015-12-08 19:50:43 +0000

    SendResponse

--- app/scripts/controllers/buttonController.js
+++ app/scripts/controllers/buttonController.js
@@ -1,20 +1,52 @@
 'use strict';
 
 angular.module('SoSafe')
-  .controller('ButtonController', function(){
+  .controller('ButtonController', function() {
+
+    var requestRef = new Firebase('https://sosafe.firebaseio.com/');
     var self = this;
+    self.user = 'Sam'
+    requestRef.child('requests').on("child_added", function(snapshot) {
+      var request = snapshot.val();
+      console.log(request);
+      if (request.receivers.includes(self.user)) {
+        self.status.message = request.sender + " wants to know if you're okay"
+      }
+    });
+
     self.status = {
       message: 'Are you ok?'
     };
 
-    self.sendRequest = function(){
-      var requestRef = new Firebase('https://sosafe.firebaseio.com');
+    self.sendRequest = function() {
       var child = requestRef.child('requests');
       var sender = 'Radu';
-      var receivers = ['Sam', 'Andrew'];
+      var receivers = {
+        'Sam': false,
+        'Andrew': false
+      };
 
-      child.push({ "sender": sender, "receivers": receivers });
+      child.push({
+        "sender": sender,
+        "receivers": receivers
+      });
 
       self.status.message = 'Waiting for response';
     };
+
+    self.sendResponse = function() {
+      var requests = requestRef.child('requests');
+      requests.once("value", function(snapshot) {
+        var data = snapshot.val();
+        snapshot.forEach(function(request) {
+          console.log(request.child('receivers'));
+          if (self.user in request.val().receivers) {
+            // console.log('hello world');
+            request.child('receivers').child(self.user).set(true);
+            self.status.message = "I'm ok!";
+          }
+        });
+      });
+    };
+
   });
