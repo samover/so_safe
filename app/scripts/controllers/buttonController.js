ff47b03c2f61556ae300b023751020752b838d39
samover <samuel.overloop@gmail.com>
2015-12-09 08:16:15 +0000

    add sendResponse

--- app/scripts/controllers/buttonController.js
+++ app/scripts/controllers/buttonController.js
@@ -6,6 +6,7 @@ angular.module('SoSafe')
     var requestRef = new Firebase('https://sosafe.firebaseio.com/');
     var self = this;
     self.user = 'Sam'
+
     requestRef.child('requests').on("child_added", function(snapshot) {
       var request = snapshot.val();
       console.log(request);
@@ -21,10 +22,15 @@ angular.module('SoSafe')
     self.sendRequest = function() {
       var child = requestRef.child('requests');
       var sender = 'Radu';
-      var receivers = {
-        'Sam': false,
-        'Andrew': false
-      };
+      var receivers = [
+        { 
+          'name': 'Sam', 
+          'status': false
+        }, {
+          'name': 'Andrew',
+          'status': false 
+        }
+      ];
 
       child.push({
         "sender": sender,
@@ -35,18 +41,22 @@ angular.module('SoSafe')
     };
 
     self.sendResponse = function() {
-      var requests = requestRef.child('requests');
-      requests.once("value", function(snapshot) {
-        var data = snapshot.val();
-        snapshot.forEach(function(request) {
-          console.log(request.child('receivers'));
-          if (self.user in request.val().receivers) {
-            // console.log('hello world');
-            request.child('receivers').child(self.user).set(true);
-            self.status.message = "I'm ok!";
+      var receiversRef = new Firebase('https://sosafe.firebaseio.com/requests');
+      
+      receiversRef.orderByKey().on('child_added', function(snapshot) {
+        var request = snapshot.val();
+        var key = snapshot.key();
+        var postRef = new Firebase(receiversRef.toString() + '/' + key);
+
+        for(var i = 0; i < request.receivers.length; i++) {
+          if(request.receivers[i].name === self.user) {
+            request.receivers[i].status = true; 
           }
-        });
+        }
+        
+        postRef.update(request);
       });
+      self.status.message = 'I am OK';
     };
 
   });
