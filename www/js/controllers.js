5cf7f41cd9e79628972977ed545587a6b2118ffa
ALRW <awerner1@googlemail.com>
2015-12-11 10:13:04 +0000

    setup App controller

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('starter.controllers', [])
+angular.module('SoSafe.controllers', [])
 
 .controller('DashCtrl', function($scope) {})
 
--- www/js/services.js
+++ www/js/services.js
@@ -1,4 +1,4 @@
-angular.module('starter.services', [])
+angular.module('SoSafe.services', [])
 
 .factory('Chats', function() {
   // Might use a resource here that returns a JSON array
